bf3e71309e6443d2521d5ad7ef92c6e789393330
maulanaihsan15 <145409988+maulanaihsan15@users.noreply.github.com>
2023-09-19 08:29:01 +0700

    Add files via upload

---
+++ latihan_1_ulang.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class latihan_1_ulang {
+     public static void main(String[]args){
+        int a=10;
+        short s=2;
+        byte b=6;
+        long l=125362133;
+        float f=65.20298f;
+        double d=876.765d;
+       System.out.println("The integers variable is "+a);
+       System.out.println("The short variable is "+s);
+       System.out.println("The variable is "+b);
+       System.out.println("The long variable is "+l);
+       System.out.println("The float variable is "+f);
+       System.out.println("The double variabele is "+d);
+       
+    }
+}
