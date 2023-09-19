4f3bffcfcd4f5c1d656c2a1de9f62ba97c0fd06d
maulanaihsan15 <145409988+maulanaihsan15@users.noreply.github.com>
2023-09-19 16:30:37 +0700

    Add files via upload

---
+++ tabel.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class tabel {
+     public static void main(String[] args) {
+        int number = 10;
+        char letter = 'A';
+        boolean result = true;
+        String str = "hello";
+        
+        System.out.println(number);
+        System.out.println(letter);
+        System.out.println(result);
+        System.out.println("hello");
+        
+    }
+}
+    
+
