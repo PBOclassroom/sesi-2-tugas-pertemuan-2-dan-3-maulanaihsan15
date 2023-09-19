9549ef610c75df65bbe394c5cbd4629f9050ba07
maulanaihsan15 <145409988+maulanaihsan15@users.noreply.github.com>
2023-09-19 08:14:43 +0700

    Add files via upload

---
+++ campuran.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class campuran {
+    public static void main(String args[]){
+        System.out.println("di sini akan membuat komentar 1 saja");
+        //komentar garis pertama
+        System.out.println("di sini saya membuat banyak garis komentar");
+        /*comentar garis ke dua
+        comentar garis ke tiga
+        cometar garis ke empat
+        comentar garis kelima
+        */
+        
+        String nama ="maulana ihsan";
+        String Nama ="Maulana Ihsan";
+        String NAMA ="MAULANA IHSAN";
+        
+        System.out.println(nama);
+        System.out.println(Nama);
+        System.out.println(NAMA);
+    }
+    
+}
