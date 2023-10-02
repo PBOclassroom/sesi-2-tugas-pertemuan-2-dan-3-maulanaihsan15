8306afd6932f80c3a3b81773557dc6b287868bea
maulanaihsan15 <145409988+maulanaihsan15@users.noreply.github.com>
2023-10-02 14:35:21 +0700

    tugas maulana ihsan

---
+++ tugas_2.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Maulana 
+ */
+public class tugas_2 {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 23;
+        int number3 = 5;
+
+        System.out.println("number 1 = " + number1);
+        System.out.println("number 2 = " + number2);
+        System.out.println("number 3 = " + number3);
+
+        int maxNumber = (number1 > number2) ? ((number1 > number3) ? number1 : number3) : ((number2 > number3) ? number2 : number3);
+
+        System.out.println("Nilai tertingginya adalah angka = " + maxNumber);
+    }
+}
+    
+
