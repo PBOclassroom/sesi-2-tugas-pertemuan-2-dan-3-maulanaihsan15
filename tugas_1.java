0841618ad75e4941c3290238f4a1e4e6f176abcb
maulanaihsan15 <145409988+maulanaihsan15@users.noreply.github.com>
2023-10-02 14:32:52 +0700

    tugas maulana ihsan

---
+++ tugas_1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class tugas_1 {
+    public static void main(String[]args){
+        int nmb1 = 10;
+        int nmb2 = 20;
+        int nmb3 = 45;
+        
+        System.out.println("Number 1 = " +nmb1);
+        System.out.println("Number 2 = " +nmb2);
+        System.out.println("Number 3 = " +nmb3);
+        System.out.println("Averege is = " +((nmb1 + nmb2 + nmb3)/3));
+    }
+
+}
